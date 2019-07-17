b5632a3194314a160c6f7a691cb460a6bec090af
Meg Oshima <mcoshima@coastal.edu>
2019-07-17 15:33:11 -0500

    RP work

--- R scripts/spr.check.R
+++ R scripts/spr.check.R
@@ -25,9 +25,69 @@ for(i in spr.seq){
     
   }
   
-  
-}
-
 
 
 # Need to check that you are at 30% every time
+mean_spr <- report.$derived_quants %>% filter(str_detect(LABEL, "Bratio")) %>% slice(tail(row_number(), 10)) %>% summarise(mean_spr = mean(Value))
+
+
+## Calculating RPs
+#### SPR = SSBy/SSB0
+
+#Virign SSB
+SSB0 <- rep.$timeseries %>% 
+  slice(1) %>% 
+  select(SpawnBio) %>% 
+  pull()
+
+#End year F
+F_cur <- rep.$derived_quants %>% 
+  filter(str_detect(LABEL, "F_")) %>% 
+  filter(str_detect(LABEL, paste(year))) %>% 
+  select(Value) %>% 
+  pull()
+
+#average of F from terminal 10 years of forecast
+equ_SPR <- rep.$derived_quants %>% 
+  filter(str_detect(LABEL, "Bratio")) %>% 
+  slice(tail(row_number(), 10)) %>% 
+  summarise(mean(Value)) %>% 
+  pull()
+
+#F at SPR 30% 
+Fspr30 <- rep.$derived_quants %>% 
+  filter(str_detect(LABEL, "F_")) %>% 
+  slice(tail(row_number(), 10)) %>% 
+  summarise(mean(Value)) %>% 
+  pull()
+
+F_ratio <- F_cur/Fspr30
+
+SSB_equ <- rep.$derived_quants %>%
+  filter(str_detect(LABEL, "SPB")) %>%
+  slice(tail(row_number(), 10)) %>%
+  summarise(mean(Value)) %>% 
+  pull()
+
+#Achieved SPR 30%? If > .3 then yes
+if(SSB_equ/SSB0 < 0.3){
+  
+  #create rebuilding plan
+
+  }
+  
+MSST <- (1-.25)*SSB_equ
+  
+rep.$derived_quants %>% filter(str_detect(LABEL, "SPB_2014"))
+
+
+
+
+
+
+
+
+
+
+
+
